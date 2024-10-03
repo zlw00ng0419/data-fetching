0a494c94ef2cf74645d1ce69ca91f51b3e7788ce
zlw00ng0419 <anggodji2422@snu.ac.kr>
2024-10-03 14:30:41 +0900

    수정

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,24 @@
 export const fetchPosts = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    return (await response.json()) as Post[];
-  };
-  
-  export type Post = {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-  };
-  
-  export const fetchPostDetail = async (postId: number) => {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts/${postId}/comments`,
-    );
-    return (await response.json()) as Comment[];
-  };
-  
-  export type Comment = {
-    id: number;
-    email: string;
-    body: string;
-  };
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  return (await response.json()) as Post[];
+};
+
+export type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+export const fetchPostDetail = async (postId: number) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}/comments`,
+  );
+  return (await response.json()) as Comment[];
+};
+
+export type Comment = {
+  id: number;
+  email: string;
+  body: string;
+};
